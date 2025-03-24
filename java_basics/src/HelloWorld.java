06be64fc2e414c207cdcdb97c993c2fe99dda55c
BigGletus <nolan@sowers.cc>
2025-03-24 20:18:20 +0000

    assignment complete

--- java_basics/src/HelloWorld.java
+++ java_basics/src/HelloWorld.java
@@ -1,5 +1,22 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Nolan"); //outputs name
+        System.out.println("Sushi"); //outputs favorite food
+        System.out.println("Immanuel Kant"); //outputs famous person i wanna meet
+        System.out.println("Mowing the lawn"); //outputs least favorite chore
+        System.out.println("Coding a game project i've been working on it Godot"); //outputs what id rather be doing
     }
 }
+
+/*
+"What does the main() method represent in a Java program?"
+    It functions as the "starting point" for a function.
+
+"Why is System.out.println() used to display output?"
+    It is a simple and universal way to convey output for debugging
+
+"What is the purpose of comments in the program?"
+    To explain the function of a block of code at a glance to save time and energy for people
+    who may not be particularly familiar with the code
+ */
